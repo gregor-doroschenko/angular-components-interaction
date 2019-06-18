5ca9e110d9b5f399162e40c76bdf24b601f128a9
Gregor Doroschenko <gregor.doroschenko@develappers.de>
2019-06-18 09:50:35 +0200

    create first component for starting point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartComponent } from './start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full'
+  },
+  {
+    path: 'start',
+    component: StartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
