98bf0fcfbc5b46397278dccd8bf083253cc456fd
Gregor Doroschenko <gregor.doroschenko@develappers.de>
2019-06-24 15:02:53 +0200

    add example for template variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { FormsModule } from '@angular/forms';
     CardModule,
     InputTextModule,
     DropdownModule,
+    ButtonModule,
     FormsModule,
     AppRoutingModule
   ],
