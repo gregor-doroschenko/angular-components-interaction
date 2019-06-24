9df1c866f91639de2b13c390f31c55d93fcd4e90
Gregor Doroschenko <gregor.doroschenko@develappers.de>
2019-06-24 16:50:10 +0200

    add example for view child

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
-export class StartComponent {
+export class StartComponent implements AfterViewInit {
 
   name = 'Gregor Doroschenko';
   userStatus: string;
+
+  @ViewChild('userProfile', { static: true })
+  userProfile: UserProfileComponent;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.userProfile.changeStatus({name: 'changed from life cycle hook'});
+    }, 3000);
+  }
+
+  setName() {
+    this.userProfile.name = 'Max Mustermann';
+  }
 
 }
